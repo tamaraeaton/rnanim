d41763afed6ceb0d2a58df001f71959e9f1ff6f1
tamaraeaton <teaton@banyanlabs.io>
2021-04-19 14:32:45 -0700

    Completed #50

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+import AnimOne from './src/anim_one';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header
+        centerComponent={{ text: 'Anim project', style: { color: '#fff' } }}
+      />
+  <AnimOne/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/anim_one.js
@@ -0,0 +1,83 @@
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, Text, View, Animated, Easing, Button } from 'react-native';
+
+
+const AnimOne = () => {
+    const movEl = useRef(new Animated.ValueXY()).current;
+    const moveElTwo = useRef(new Animated.Value(1)).current;
+
+//  const runMyAnim = () => {
+//         Animated.timing(movEl, {
+//             toValue: { x: 100, y: 300 },
+//             duration:2000,
+//             delay:1000,
+//             easing:Easing.elastic(1)
+//         }).start();
+//     }
+    const runMyTimingAnim = () => {
+        Animated.timing(moveElTwo, {
+            toValue:0,
+            duration:1000,
+            delay:1000
+            
+        }).start();
+    }
+
+    return (
+        <View style={styles.container}>
+            <Button 
+                title="Trigger anim"
+                onPress={runMyTimingAnim}
+            />
+            <Animated.View style={{
+                // opacity: moveElTwo,
+    // StandUp Begin
+                // transform:[
+                //     {
+                //         rotateX:moveElTwo.interpolate({
+                //             inputRange:[0, 0.5, 1],
+                //             outputRange:['0deg', '180deg', '0deg']
+                //         })
+                //     }
+                // ]
+    // StandUp End
+                // left:moveElTwo.interpolate({
+                //     inputRange:[0, 1],
+                //     outputRange:[300, 0]
+                // })
+            }}>
+                <View style={styles.square}>
+                    <Animated.Text
+                        style={{
+                            fontSize:moveElTwo.interpolate({
+                                inputRange:[0, 0.5, 0.8, 1],
+                                outputRange:[25, 5, 10, 30]
+                            }),
+                            color:moveElTwo.interpolate({
+                                inputRange:[0, 1],
+                                outputRange:['blue', 'green']
+                            })
+                        }}
+                    >
+                        <Text>Anim one</Text>
+                    </Animated.Text>
+                    
+                </View>
+            </Animated.View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        //flex:1,
+        backgroundColor: '#ffffff'
+    },
+    square: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red'
+    }
+})
+
+export default AnimOne;
